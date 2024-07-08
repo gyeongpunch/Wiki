864f83dd47c5fe3925301373d7ae1383bcfe72bf
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2024-07-08 22:06:54 +0900

    Add files via upload

---
+++ missions/W2M1.py
@@ -0,0 +1,23 @@
+import multiprocessing
+import time
+
+# list of tasks with name and duration
+tasks = [
+    ('A', 5),
+    ('B', 2),
+    ('C', 1),
+    ('D', 3),
+]
+
+# Simulate each task
+def work_log(task):
+    name, duration = task
+    print(f"Process {name} waiting {duration} seconds")
+    time.sleep(duration)
+    print(f"Process {name} Finished.")
+
+if __name__ == "__main__":
+    # Pool with len(tasks) workers
+    with multiprocessing.Pool(len(tasks)) as pool:
+        # Map the work_log function to the list of tasks
+        pool.map(work_log, tasks)
